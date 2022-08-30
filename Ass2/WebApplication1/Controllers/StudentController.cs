cba8694c30709bcdb75a786671d25c1a3dac84a0
trungmark <trungbui0121@gmail.com>
2022-08-30 16:24:09 +0700

    ass2

---
+++ Ass2/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["Name"] = "Bui Van Trung";
+            TempData["Age"] = 23;
+            TempData["Address"] = "Ha Noi,VietNam";
+            return View("ShowData");
+        }
+
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+    }
+}
